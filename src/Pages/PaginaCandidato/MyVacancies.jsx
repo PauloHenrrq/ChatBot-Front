4b595e6f0dc426a2037d40fd9f27668de9cf6a4b
Levi <duartelevi1@gmail.com>
2025-05-06 14:12:42 -0300

    .

--- src/Pages/PaginaCandidato/MyVacancies.jsx
+++ src/Pages/PaginaCandidato/MyVacancies.jsx
@@ -1,12 +1,52 @@
-
-
+import { useState, useEffect } from "react"
+import HeaderCandidato from "../../Layout/HeaderCandidato"
+import { api } from "../../Routes/server/api"
 
 export default function MyVacancies() {
+    const [vagas, setVagas] = useState([])
+    const [candidato, setCandidato] = useState([])
 
-    return(
-        <>
-        <p>a</p>
+    useEffect(() => {
+        const carregarCandidato = async () => {
+            try {
+                const response = await api.get(`/candidaturas`)
+                setCandidato(response.data)
+                console.log(response.data) // Aqui você verá todas as vagas
+            } catch (error) {
+                console.error('Erro ao carregar candidatos:', error)
+            }
+        }
         
+        
+        carregarCandidato()
+        
+    }, [])
+
+    useEffect(() => {
+        const carregarVagas = async (candidato) => {
+            try {
+                const response = await api.get(`/vagas/${candidato.vagaId}`)
+                setVagas(response.data)
+                console.log(response.data)
+            } catch (error) {
+                console.error('Erro ao carregar vagas:', error)
+            }
+        }
+        carregarVagas()
+    }, [])
+
+
+    return (
+        <>
+            <HeaderCandidato />
+
+            <section className="max-w-6xl mx-auto mt-10">
+                <div>
+                   {candidato.map((cand) => {
+                    <h1></h1>
+                   })}
+                </div>
+            </section>
         </>
     )
 }
